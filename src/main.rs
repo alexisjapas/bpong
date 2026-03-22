6d2a32cee4e0c8b874e7c53731261b19a57f096b
alexisjapas <alexis.japas.dev@pm.me>
2026-03-22 16:11:54 +0100

    feat: other entities

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() {
         ..default()
     }));
 
-    app.add_systems(Startup, (setup_camera, spawn_player));
+    app.add_systems(Startup, (setup_camera, spawn_player, spawn_ai, spawn_ball));
 
     app.add_systems(Update, move_player);
 
@@ -22,6 +22,12 @@ fn main() {
 // Components
 #[derive(Component)]
 struct Player;
+
+#[derive(Component)]
+struct Ai;
+
+#[derive(Component)]
+struct Ball;
 
 #[derive(Component)]
 struct Health(u32);
@@ -39,6 +45,31 @@ fn spawn_player(mut commands: Commands) {
         Sprite {
             color: Color::srgb(0.5, 0.5, 0.5),
             custom_size: Some(Vec2::new(32.0, 64.0)),
+            ..default()
+        },
+    ));
+}
+
+fn spawn_ai(mut commands: Commands) {
+    commands.spawn((
+        Ai,
+        Health(10),
+        Transform::from_xyz(328.0, 0.0, 0.0),
+        Sprite {
+            color: Color::srgb(0.5, 0.5, 0.5),
+            custom_size: Some(Vec2::new(32.0, 64.0)),
+            ..default()
+        },
+    ));
+}
+
+fn spawn_ball(mut commands: Commands) {
+    commands.spawn((
+        Ball,
+        Transform::from_xyz(0.0, 0.0, 0.0),
+        Sprite {
+            color: Color::srgb(1.0, 1.0, 1.0),
+            custom_size: Some(Vec2::new(16.0, 16.0)),
             ..default()
         },
     ));
