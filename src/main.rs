a6bc1c3075447a43b090c90e63716cc528e26206
alexisjapas <alexis.japas.dev@pm.me>
2026-03-22 17:13:47 +0100

    feat: clamp player positions

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use bevy::prelude::*;
 use bevy::window::WindowResolution;
 
+const SCREEN_WIDTH: f32 = 720.;
+const SCREEN_HEIGHT: f32 = 480.;
+const PLAYER_HEIGHT: f32 = 64.;
+const PLAYER_WIDTH: f32 = 32.;
+const BALL_SIZE: f32 = 16.;
+
 fn main() {
     let mut app = App::new();
     app.add_plugins(DefaultPlugins.set(WindowPlugin {
         primary_window: Some(Window {
             title: "Bin pong".into(),
-            resolution: WindowResolution::new(720, 480),
+            resolution: WindowResolution::new(SCREEN_WIDTH as u32, SCREEN_HEIGHT as u32),
             ..default()
         }),
         ..default()
@@ -41,10 +47,10 @@ fn spawn_player(mut commands: Commands) {
     commands.spawn((
         Player,
         Health(10),
-        Transform::from_xyz(-328.0, 0.0, 0.0),
+        Transform::from_xyz(-SCREEN_WIDTH / 2. + PLAYER_WIDTH, 0.0, 0.0),
         Sprite {
             color: Color::srgb(0.5, 0.5, 0.5),
-            custom_size: Some(Vec2::new(32.0, 64.0)),
+            custom_size: Some(Vec2::new(PLAYER_WIDTH, PLAYER_HEIGHT)),
             ..default()
         },
     ));
@@ -57,7 +63,7 @@ fn spawn_ai(mut commands: Commands) {
         Transform::from_xyz(328.0, 0.0, 0.0),
         Sprite {
             color: Color::srgb(0.5, 0.5, 0.5),
-            custom_size: Some(Vec2::new(32.0, 64.0)),
+            custom_size: Some(Vec2::new(PLAYER_WIDTH, PLAYER_HEIGHT)),
             ..default()
         },
     ));
@@ -69,7 +75,7 @@ fn spawn_ball(mut commands: Commands) {
         Transform::from_xyz(0.0, 0.0, 0.0),
         Sprite {
             color: Color::srgb(1.0, 1.0, 1.0),
-            custom_size: Some(Vec2::new(16.0, 16.0)),
+            custom_size: Some(Vec2::new(BALL_SIZE, BALL_SIZE)),
             ..default()
         },
     ));
@@ -83,10 +89,18 @@ fn move_player(
     const SPEED: f32 = 256.;
     for mut player_transform in query.iter_mut() {
         if input.pressed(KeyCode::ArrowUp) {
-            player_transform.translation.y += SPEED * time.delta_secs();
+            player_transform.translation.y =
+                (player_transform.translation.y + SPEED * time.delta_secs()).clamp(
+                    -SCREEN_HEIGHT / 2. + PLAYER_HEIGHT / 2.,
+                    SCREEN_HEIGHT / 2. - PLAYER_HEIGHT / 2.,
+                );
         }
         if input.pressed(KeyCode::ArrowDown) {
-            player_transform.translation.y -= SPEED * time.delta_secs();
+            player_transform.translation.y =
+                (player_transform.translation.y - SPEED * time.delta_secs()).clamp(
+                    -SCREEN_HEIGHT / 2. + PLAYER_HEIGHT / 2.,
+                    SCREEN_HEIGHT / 2. - PLAYER_HEIGHT / 2.,
+                );
         }
     }
 }
